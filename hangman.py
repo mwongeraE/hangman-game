48fbdd459c6468c800c2cd43074f3cf511a836d3
mwongeraE <evansmwongera@gmail.com>
2016-02-12 03:16:28 +0300

    chooses start

--- hangman.py
+++ hangman.py
@@ -1,1 +1,15 @@
-class hangman (): #create class
+class hangman ():#create class
+    def __init__(self):
+        print "hangman., ready?"
+        print "(1)yes. \n(2)no."
+        user_choice_1 = raw_input("->") #user choice
+
+        if user_choice_1 == '1':
+            print "loading"
+            self.start_game()
+        elif user_choice_1 == '2':
+            print "goodbye"
+            exit()
+        else:
+            self.__init__()
+
